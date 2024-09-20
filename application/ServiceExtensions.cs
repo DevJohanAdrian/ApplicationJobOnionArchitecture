29cd1a63826ad2e0ffb8805b4eea265480663bff
DevJohanAdrian <jamaringarciabusiness@gmail.com>
2024-09-19 19:43:55 -0500

    Agregar archivos de proyecto.

---
+++ application/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿
+
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;  //IServiceCollection
+using System.Reflection; //ASSEMBLY
+
+
+namespace application
+{
+    internal static class ServiceExtensions
+    {
+        public static void AddApplicationLayer(this IServiceCollection services) {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly()); //para CQRS
+        }
+    }
+}
